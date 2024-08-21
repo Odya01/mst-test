0b0ff8d157107af19810b95d4710fb1b06c50349
Odya01 <republiquez25@gmail.com>
2024-08-21 17:00:07 +0300

    fixed bug

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ checkbox.addEventListener('change', function () {
     submitBtn.disabled = true;  // Отключаем кнопку
   }
 });
+
+
+
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
